d5dd00b9ce3a3930d1eb4f10da31ff46d5087cb4
Jeff Hui <jeff@jeffhui.net>
2015-08-26 01:33:09 -0700

    Added everything

---
+++ NimbleCocoaPodsExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NimbleCocoaPodsExample
+//
+//  Created by Jeff Hui on 1/12/15.
+//  Copyright (c) 2015 Jeff Hui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '8.0'
+
+use_frameworks!
+
+target 'NimbleCocoaPodsExample' do
+  pod 'Nimble', :path => '../Nimble' #:git => "https://github.com/Quick/Nimble"
+end
+
+target 'NimbleCocoaPodsExampleTests' do
+  pod 'Quick', :git => 'https://github.com/Quick/Quick', :tag => 'v0.5.1'
+  pod 'Nimble', :path => '../Nimble' #:git => "https://github.com/Quick/Nimble"
+end
+
